7de71c7ee9cacaf0c8f45a4b17fd16cada328dec
alexandriaware <ali.mercer92@gmail.com>
2017-10-10 10:27:27 -0400

    created Boat class

---
+++ Week3_Day2/Boat.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_Day2
+{
+    class Boat
+    {
+        //fields - describes characteristics of properties -- information about the specific instances
+        public int numberOfPassengers;
+            
+        private string name;
+        private double engineSize;
+
+
+        //Property - 
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+
+        //Constructors -- used to create something 
+        public Boat()
+        {
+
+        }
+
+        public Boat(int numberOfPassengers, string name, double engineSize)
+        {
+            this.numberOfPassengers = numberOfPassengers;
+            this.name = name;
+            this.engineSize = engineSize;
+        }
+
+
+        //Methods
+        public void Move()
+        {
+            Console.WriteLine(name + " moves forward.");
+        }
+
+        private double CalculateMPG() // <---only code inside the Boat class can use this bc Private
+        {
+            double MPG = engineSize * .7d;
+
+            return MPG;
+        }
+
+    }
+}
